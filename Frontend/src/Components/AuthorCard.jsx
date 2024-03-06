ad3889ce86a83d057909129806229c5145bb4544
Md. Mosharaf Hossain Apurbo <aoarish397@gmail.com>
2024-03-06 23:24:11 +0600

    product card updated

--- Frontend/src/Components/AuthorCard.jsx
+++ Frontend/src/Components/AuthorCard.jsx
@@ -4,26 +4,35 @@ import Image from 'react-bootstrap/Image';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import './AuthorCard.css'; // Import CSS file
+import Button from 'react-bootstrap/Button';
 
 
 function AuthorCard(props) {
 
-    const { IMAGE, NAME, ID ,link} = props; // Assuming you have an id property in the props
+    const { IMAGE, NAME, ID, link } = props; // Assuming you have an id property in the props
 
-    const path ="/"+ link +"/"+ ID; // Assuming you have a path property in the props
+    const path = "/" + link + "/" + ID; // Assuming you have a path property in the props
 
     return (
 
 
 
-        <Card className='custom-author-card' style={{ padding: '20px', marginRight: '20px', marginTop: '10px' ,marginBottom:'10px',backgroundColor:'#ffffe4'  }}>
+        <Card className='custom-author-card' style={{ padding: '20px', marginRight: '20px', marginLeft:'15px', marginTop: '10px', marginBottom: '10px', backgroundColor: '#ffffe4' }}>
             <Container fluid className="text-center">
-                <div className='ring' style={{ border: '3px solid blue', borderRadius: '50%', display: 'inline-block', padding: '5px' }}>
+                <div className='ring' style={{ border: '3px solid green', borderRadius: '50%', display: 'inline-block', padding: '5px' }}>
                     <Image src={IMAGE} roundedCircle className="img-fluid" style={{ width: '140px', height: '135px' }} />
                 </div>
                 <Card.Text className="mt-3">{NAME}</Card.Text>
                 {/* Use Link component instead of Button */}
-                <Link to={path} className="btn btn-warning btn-md">View Profile</Link>
+                <Link to={path} >
+                    <Button variant='warning'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                            <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
+                            <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
+                        </svg>
+                        View Profile
+                    </Button>
+                </Link>
             </Container>
         </Card>
     );
--- Frontend/src/Components/Card.jsx
+++ Frontend/src/Components/Card.jsx
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import './Card.css'; // Import CSS file
-import { taka } from '../Pages/Constants';
+// import { taka } from '../Pages/Constants';
 
 function CustomCard(props) {
 
@@ -19,10 +19,34 @@ function CustomCard(props) {
           {/* <Button variant="primary">Add to Cart</Button> */}
           {/* Use Link component instead of React Bootstrap's Link */}
           {/* // Remove the href attribute from the anchor tag for the eye icon */}
+          <Link to={`/book/${props.ID}`} >
+            <Button variant='outline-warning' style={{ marginRight: '10px' }}>
+
+              <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
+                <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
+              </svg>
+            </Button>
+          </Link>
+
+
+          <Button variant='outline-danger' style={{ marginRight: '10px' }}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+              <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
+            </svg>
+          </Button>
+
+          <Button variant='outline-success' >
+            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-bag-plus" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5" />
+              <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z" />
+            </svg>
+
+          </Button>
 
 
 
-          <Link to={`/book/${props.ID}`} className="btn btn-warning btn-md " style={{ marginLeft: '5px' }}>View Book</Link>
+
         </Card.Body>
       </Card>
     </div>
